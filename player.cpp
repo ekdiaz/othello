68b56e7da137a44fc0ea5324b74653511f8615c6
ekdiaz <ediaz@caltech.edu>
2017-03-01 21:48:32 -0800

    Add heuristic scorer (AI now beats SimplePlayer)

--- player.cpp
+++ player.cpp
@@ -36,21 +36,88 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
     board.doMove(opponentsMove, getOpponent());
     if (board.hasMoves(side))
     {
+    	//Working AI
+    // 	for (int x = 0; x < 8; x++)
+    // 	{
+    // 		for (int y = 0; y < 8; y++)
+    // 		{
+    // 			Move *m = new Move(x, y);
+				// if (board.checkMove(m, side))
+				// {
+				// 	board.doMove(m, side);
+				// 	return m;
+				// }
+				// delete m;
+    // 		}
+    // 	}
+    	//AI that beats SimplePlayer
+    	Move *best = nullptr;
+    	int bestScore = -100000000;
     	for (int x = 0; x < 8; x++)
     	{
     		for (int y = 0; y < 8; y++)
     		{
     			Move *m = new Move(x, y);
-				if (board.checkMove(m, side))
-				{
-					board.doMove(m, side);
-					return m;
-				}
-				delete m;
+    			if (board.checkMove(m, side))
+    			{
+    				int score = getScore(m);
+    				if (score > bestScore)
+    				{
+    					best = m;
+    					bestScore = score;
+    				}
+    			}
     		}
     	}
+    	board.doMove(best, side);
+    	return best;
+
     }
     return nullptr;
+}
+
+/**
+ * @brief Calculates heuristic score of a given valid move.
+ * 
+ * @returns int score of move.
+ */
+int Player::getScore(Move *m)
+{
+	Board *bcopy = this->board.copy();
+	bcopy->doMove(m, side);
+	int score = bcopy->count(side) - bcopy->count(getOpponent());
+	int x = m->getX();
+	int y = m->getY();
+	//if one of the corners, more valuable
+	if (((x == 0) && (y == 0)) || ((x == 7) && (y == 0)) || ((x == 0) && (y == 7)) || ((x == 7) && (y == 7)))
+	{
+		score *= 3;
+	}
+
+	//if adjacent to one of the corners, less valuable
+	//top left
+	else if (((x == 1) && (y == 0)) || ((x == 1) && (y == 1)) || ((x == 0) && (y == 1)))
+	{
+		score *= -3;
+	}
+	//top right
+	else if (((x == 6) && (y == 0)) || ((x == 6) && (y == 1)) || ((x == 7) && (y == 1)))
+	{
+		score *= -3;
+	}
+	//bottom left
+	else if (((x == 0) && (y == 6)) || ((x == 1) && (y == 6)) || ((x == 1) && (y == 7)))
+	{
+		score *= -3;
+	}
+	//bottom right
+	else if (((x == 7) && (y == 6)) || ((x == 6) && (y == 6)) || ((x == 6) && (y == 7)))
+	{
+		score *= -3;
+	}
+
+	delete bcopy;
+	return score;
 }
 
 /**
